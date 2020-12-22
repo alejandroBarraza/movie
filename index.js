10a8ca2af95c67347b13eaa13121456130a682d9
alejandro <alejandro.barraza@alumnos.ucn.cl>
2020-12-21 23:19:06 -0300

    fetch movie

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+//fetch api with axios.
+const fetchData = async () => {
+  const resp = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "ec9bde75",
+      s: "avengers",
+    },
+  });
+  console.log(resp.data);
+};
+fetchData();
