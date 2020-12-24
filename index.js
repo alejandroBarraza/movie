0a4745107b7e6562602c0a9bc1512ce5edb91544
alejandro <alejandro.barraza@alumnos.ucn.cl>
2020-12-24 01:01:19 -0300

    add new things as displaymovies in html after search

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 //fetch api with axios.
-const fetchData = async () => {
+const fetchData = async (searchMovie) => {
   const resp = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "ec9bde75",
-      s: "avengers",
+      s: searchMovie,
     },
   });
-  console.log(resp.data);
+  return resp.data.Search;
 };
-fetchData();
+
+//select the input
+const input = document.querySelector(".container input ");
+
+//function than display poster and title from a fetch movie.
+const displayMovies = (movies) => {
+  for (const movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h2>${movie.Title}</h2>
+    <img src="${movie.Poster}">
+    `;
+    document.querySelector("#target").append(div);
+  }
+};
+
+//fuction fetch a movie each time user type.
+const onInput = async ({ target }) => {
+  //async beacuse movies should wait for fectch the movie.
+  const movies = await fetchData(target.value); //movies constains the arrays of list movies
+  //console.log(movies);
+  displayMovies(movies);
+};
+
+//events that each times textValue change makes a fetch with debounce at 500ms delate.
+input.addEventListener("input", debounce(onInput, 500));
