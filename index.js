430cd852f7ccde92a448196ad52a696fb78607dd
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-01-03 01:48:14 -0300

    refactor file as a not reusable code unique from app

--- index.js
+++ index.js
@@ -1,102 +1,50 @@
-// const displayMoviError = (error) => {
-//   const div = document.createElement("div");
-//   return (div.innerHTML = `
-//   <p>${error}</p>
-//   `);
-// };
-
-//fetch api with axios.
-const fetchData = async (searchMovie) => {
-  const resp = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "ec9bde75",
-      s: searchMovie,
-    },
-  });
-  //return a empty array until make the dropdown seatchbar.(FIX AFTER)
-  if (resp.data.Error) {
-    return [];
-  }
-  return resp.data.Search;
-};
-//select the root elemt for append results
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-  <label><b> Search for an Anime </label>
-  <input class="input"/>
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results">  
-      </div>
-    </div>         
-  </div>
-
-`;
-//select the input
-const input = document.querySelector(".container input ");
-//select the dropdown div for show the dropmenu or not.
-const dropdown = document.querySelector(".dropdown");
-//select the reulst for display the content.
-const results = document.querySelector(".results");
-//select the dropdown menu
-const menu = document.querySelector(".dropdown-menu");
-
-//function than display poster and title from a fetch movie.
-const displayMovies = (movies) => {
-  //before display more movies.clean the dropdown content.
-  //for the first time will me empty so doenst matters.
-  results.innerHTML = "";
-
-  for (const movie of movies) {
-    const anchor = document.createElement("a"); //anchor bc bulma doc is an anchor for dropmenu.
-    anchor.classList.add("dropdown-item"); //css class for dropdown in bulmca documentation.
+//reusable object to pass in to CreateAutocomplete object.
+const autocomplete = {
+  renderOption(movie) {
     //check for broken images comming from API.
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-    anchor.innerHTML = `
-      <img src="${imgSrc}">
-      <h2>${movie.Title}</h2>
-    `;
-    results.append(anchor);
-
-    anchor.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie.imdbID);
+    return `
+    <img src="${imgSrc}">
+    <h2>${movie.Title}</h2>
+  `;
+  },
+  onOptionSelect(movie) {
+    //before render the movie data.hide tutorial banner.
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async onFetchData(searchTerm) {
+    const resp = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "ec9bde75",
+        s: searchTerm,
+      },
     });
-  }
+    //return a empty array until make the dropdown seatchbar.(FIX AFTER)
+    if (resp.data.Error) {
+      return [];
+    }
+    return resp.data.Search;
+  },
 };
 
-//fuction fetch a movie each time user type.
-const onInput = async ({ target }) => {
-  //async beacuse movies should wait for fectch the movie.
+//first input search
 
-  const movies = await fetchData(target.value); //movies constains the arrays of list movies
-
-  //if i do the fetch and got an empty array .close the dropmenu.
-  if (!movies.length) {
-    //contains a empty array.
-    dropdown.classList.remove("is-active");
-    return; //return for dont enter in the another part of the code.
-  }
-  //console.log(movies);
-  dropdown.classList.add("is-active");
-  displayMovies(movies);
-};
-
-//events that each times textValue change makes a fetch with debounce at 500ms delate.
-input.addEventListener("input", debounce(onInput, 500));
-
-//events to close the dropmenu is user click OUTSIDE the menu options.
-document.addEventListener("click", ({ target }) => {
-  //console.log(event.target);
-  //doing click outside root elements(root element grab all the menu)
-  if (!root.contains(target)) {
-    //if root not contains events target.means clicking outside this parent elemtent.
-    dropdown.classList.remove("is-active");
-  }
+createAutocomplete({
+  root: document.querySelector("#left-autocomplete"),
+  ...autocomplete,
 });
 
-//second fetch for after movie seleecion and see full details.
+//second input search.
+createAutocomplete({
+  root: document.querySelector("#right-autocomplete"),
+  ...autocomplete,
+});
+
+//second fetch for after movie selection and see full details.
 const onMovieSelect = async (movie) => {
   const resp = await axios.get("http://www.omdbapi.com/", {
     params: {
